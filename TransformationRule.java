4cb0e16807683f8d9f4bb9b19974594d09fbf7de
Nguyen Manh Ha <manhhad32@gmail.com>
2024-09-13 10:20:03 +0700

    change name variable

--- TransformationRule.java
+++ TransformationRule.java
@@ -3,37 +3,22 @@ package postagging;
 import java.util.List;
 // POS tagging rule
 public class TransformationRule {
-    private String conditionPos;
-    private String correctPos;
-    private String contextCondition;
+    private String posTagged;
+    private String correctTag;
+    private String contextTagged;
 
-    public TransformationRule(String conditionPos, String correctPos, String contextCondition) {
-        this.conditionPos = conditionPos;
-        this.correctPos = correctPos;
-        this.contextCondition = contextCondition;
-    }
-/* 
-    public boolean matches(List<TaggedWord> sentence, int index) {
-        // Basic rule: If the current word is tagged as 'conditionPos' and the context condition is satisfied, apply the rule
-        if (sentence.get(index).posTag.equals(conditionPos)) {
-            // Example condition: the previous word must have a specific tag, contextCondition
-            if (index > 0 && sentence.get(index - 1).posTag.equals(contextCondition)) {
-                return true;
-            }
-        }
-        return false;
+    public TransformationRule(String posTagged, String correctTag, String contextTagged) {
+        this.posTagged = posTagged;
+        this.correctTag = correctTag;
+        this.contextTagged = contextTagged;
     }
 
-    public void apply(List<TaggedWord> sentence, int index) {
-        sentence.get(index).posTag = this.correctPos;
-    }
-*/
     public void applyRule(List<TaggedWord> sentence, int index) {
-        // Basic rule: If the current word is tagged as 'conditionPos' and the context condition is satisfied, apply the rule
-        if (sentence.get(index).posTag.equals(conditionPos)) {
-            // Example condition: the previous word must have a specific tag, contextCondition
-            if (index > 0 && sentence.get(index - 1).posTag.equals(contextCondition)) {
-                sentence.get(index).posTag = this.correctPos;
+        // Basic rule: If the current word is tagged as 'posTagged' and the context condition is satisfied, apply the rule
+        if (sentence.get(index).posTag.equals(posTagged)) {
+            // Example condition: the previous word must have a specific tag, contextTagged
+            if (index > 0 && sentence.get(index - 1).posTag.equals(contextTagged)) {
+                sentence.get(index).posTag = this.correctTag;
             }
         }
     }
